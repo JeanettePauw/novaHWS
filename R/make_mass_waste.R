7a88526290f9a9770176b9e843bf3107a47f3eae
Jeanette Pauw <Jeanette Pauw>
2023-03-06 12:51:59 +0200

    Opsie om nie as indicator uit te voer nie

---
+++ R/make_mass_waste.R
@@ -0,0 +1,71 @@
+
+## mass of waste per household per week
+
+#' make_mass_waste
+#' Get the overall average by taking the average of the stand averages. This
+#' means that each stand gets the same weight (/#of stands) in the calculation.
+#' When multiplier is specified, the average is calculated per "multiplier",
+#' for example per capita.
+#' @param df tibble
+#' @param group Charcter.
+#' @param idvar Charcter.
+#' @param dataset Charcter.
+#' @param multiplier Charcter. Defalut NA
+#' @param pername Charcter.
+#' @param period Charcter.
+#' @param srcpth Charcter.
+#' @param measure Charcter.
+#' @param unt Charcter.
+#' @param category Currently ignored - for campatability with other functions in family to enable uniform arg list
+#' @param namewash Currently ignored - for campatability with other functions in family to enable uniform arg list
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' make_mass_waste(df = ls_ana$data[[1]], multiplierdf = ls_bd$data[[1]], pername = "capita", group = "CAP")
+#'
+#'
+make_mass_waste <- function(df,
+                            group = "",
+                            idvar = "ID",
+                            dataset = "HWS2022",
+                            indicatorOut = TRUE,
+                            measure = c("MASS", "VOL")[1],
+                            multiplier = c(NA, "number_members_residing")[1],
+                            pername = c("household", "capita")[1],
+                            unt = c("kg", "l")[1],
+                            period = "WEEK",
+                            srcpth = "",
+                            ...){
+
+
+  if (!is.na(multiplier)) {
+    res <- df %>% mutate(sample_weight = dry_sample_weight + wet_sample_weight) %>%
+      select({{idvar}}, {{multiplier}}, sample_weight) %>%
+      group_by(!!sym(idvar)) %>%
+      summarise(kg_pw_phh = mean( sample_weight /  !!sym(multiplier))) %>%
+      summarise(kg_pw = summaryX(kg_pw_phh, allowNegCIL = FALSE)) %>%
+      `[[`(1)
+  } else {
+    res <- df %>% mutate(sample_weight = dry_sample_weight + wet_sample_weight) %>%
+      select({{idvar}}, sample_weight) %>%
+      group_by(!!sym(idvar)) %>%
+      summarise(kg_pw_phh = mean(sample_weight)) %>%
+      summarise(kg_pw = summaryX(kg_pw_phh, allowNegCIL = FALSE)) %>%
+      `[[`(1)
+  }
+if (!indicatorOut) return(res)
+
+  indic <- new("indicator")
+  indic@name <- sprintf("%s_WASTE_P%s_P%s_%s", measure, group, period, dataset)
+  indic@type <- "continuous"
+  indic@val <- res
+  indic@unit <- unt
+  indic@description <- sprintf("Average %s of waste per %s per week. Unit: %s. Data set: %s.", tolower(measure), pername, unt, dataset)
+  indic@sourcepath <- srcpth
+  indic@date_created <- as.character(Sys.time())
+
+  #assign(x = indic@name, value = indic, envir = indicators[[st]][[twn]])
+  indic
+}
---
+++ R/summaryX.R
@@ -0,0 +1,64 @@
+#' Summary X
+#' 
+#' Summarise continuous variable
+#' 
+#' @family indicatify
+#' @family summaryFuncs
+#'
+#' @param x Numeric vector
+#' @param digits Numeric vector; indicates number of significant digits.
+#' @param NAsToZero Logical value; substitutes NA values with 0 if TRUE .
+#'   Default is FALSE.
+#' @param allowNegCIL Logical value; allows negative conf.int if TRUE.
+#' @param combinedCI Numeric value; combined conf.int
+#'
+#' @note Use with reporting function 'reportContinuous'.
+#' @export
+
+summaryX <- function(x,
+                     digits = 3,
+                     NAsToZero = FALSE,
+                     allowNegCIL = TRUE,
+                     combinedCI = FALSE) {
+        
+        # TODO: NB! tans stel dit negatiewe CILs net na 0 indien allowNegCIL == FALSE. Daar
+        # is egter 'n ordentlike manier om CIs te bereken wanneer negatiewe CILs nie
+        # moontlik is nie. Vind by CJP uit wat daardie fix is en werk dit hier in!
+        
+        if (NAsToZero) {
+                x[which(is.na(x))] <- 0
+        }
+        
+        smmry <- round(summary(x), digits = digits)
+        nms <- names(smmry)
+        smmry <- as.vector(smmry)
+        df <- data.frame(matrix(data = smmry,
+                                nrow = 1,
+                                ncol = length(smmry)))
+        names(df) <- nms
+        df$n <- length(x[which(!is.na(x))])
+        CIs <- CI(x = x, ci = 0.95, na.rm = T, as.df = TRUE)
+        if (!allowNegCIL) {
+                idxx <- which(CIs[["Lower"]] < 0)
+                if (length(idxx) > 0) {CIs[idxx,"Lower"] <- 0}
+        }
+        if (!combinedCI) {
+                df[["95% CI L."]] <- round(CIs[["Lower"]], digits = digits)
+                df[["95% CI U."]] <- round(CIs[["Upper"]], digits = digits)
+        } else {
+                if (any(!is.nan(c(CIs[["Lower"]], CIs[["Upper"]])))) {
+                        df[["95% CI"]] <- sprintf("(%s, %s)",
+                                                  round(CIs[["Lower"]], digits = digits),
+                                                  round(CIs[["Upper"]], digits = digits))
+                } else {
+                        df[["95% CI"]] <- NA_character_
+                }
+        }
+        df[["Std. Dev"]] <- round(sd(x, na.rm = TRUE), digits = digits)
+        
+        for (v in 1:ncol(df)) {
+                df[[v]][which(is.nan(df[[v]]))] <- NA_real_
+        }
+        
+        return(df)
+}
